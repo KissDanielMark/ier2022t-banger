0de8e1b5497909177c5a8aa03e2bc7e2a792e6d6
DanMark <41143582+KissDanielMark@users.noreply.github.com>
2022-05-26 18:47:50 +0200

    HouseEnv tiszta

--- konyha/HouseEnv.java
+++ konyha/HouseEnv.java
@@ -1,7 +1,6 @@
 import jason.asSyntax.*;
 import jason.environment.Environment;
 import jason.environment.grid.Location;
-
 import java.util.*;
 import java.util.logging.Logger;
 
@@ -12,20 +11,17 @@ public class HouseEnv extends Environment {
     static String currentAsztal = "ures";
     static int currentKaja = 0;
 
-    // common literals - itt van az atkapcsolas jason baszasbol
+    // common literals - itt van az atkapcsolas jason-bol
     public static final Literal openFridgeParancs  = Literal.parseLiteral("open(fridge)");
     public static final Literal closeFridgeParancs = Literal.parseLiteral("close(fridge)");
     public static final Literal getAlapAgParancs  = Literal.parseLiteral("get(alapanyag)");
     public static final Literal handInAlapAgParancs  = Literal.parseLiteral("hand_in(alapanyag)");
     public static final Literal kavarEtelParancs  = Literal.parseLiteral("kavar(etel)");
     public static final Literal hasSzakacsEtelParancs = Literal.parseLiteral("has(szakacs,alapanyag)");
-
     public static final Literal cookEtelParancs = Literal.parseLiteral("cook(etel)");
     public static final Literal doneParancs = Literal.parseLiteral("elkeszult(keszkaja)");
-
     public static final Literal atFridgeParancs = Literal.parseLiteral("at(kukta,fridge)");
     public static final Literal ao = Literal.parseLiteral("at(kukta,szakacs)");
-
     public static final Literal felveszParancs = Literal.parseLiteral("felvesz(keszkaja)");
     public static final Literal felszolgalParancs = Literal.parseLiteral("felszolgal(keszkaja)");
     public static final Literal serveParancs = Literal.parseLiteral("serve(keszkaja)");
@@ -36,24 +32,17 @@ public class HouseEnv extends Environment {
     public static final Literal pincerAtAsztal3 = Literal.parseLiteral("at(pincer,asztal3)");
 
     static Logger logger = Logger.getLogger(HouseEnv.class.getName());
-
     HouseModel model; // the model of the grid
 
     @Override
     public void init(String[] args) {
-        //MAS consolban jelenik meg ez!!!
-        //logger.info("Elindult a java");
-        System.out.println("Itt indul a JAVA program....");
         model = new HouseModel();
-
         RendeloKiosk k = new RendeloKiosk();
-        
-        if (args.length == 1 && args[0].equals("gui")) {
-            System.out.println("Kell GUI");
+        if (args.length == 1 && args[0].equals("gui")) 
+        {
             HouseView view  = new HouseView(model);
             model.setView(view);
         }
-
         updatePercepts();
     }
 
@@ -71,14 +60,12 @@ public class HouseEnv extends Environment {
                 kiszolgalasFolyamatban = true;
                 currentAsztal = r.asztal;
                 currentKaja = r.kaja;
-                System.out.println("Választott kaja: " + currentKaja);
-                System.out.println("Választott asztal: " + currentAsztal);
+                System.out.println("Ezt az etelt valasztottak: " + currentKaja+"a(z) "+currentAsztal+" asztalnál");
             }
         }
 
         addPercept("pincer", Literal.parseLiteral("rendeles("+ currentAsztal +")"));
 
-        // get the robot location
         Location locationRobot = model.getAgPos(0);
 
         // add agent location to its percepts
@@ -119,20 +106,14 @@ public class HouseEnv extends Environment {
     
     @Override
     public boolean executeAction(String ag, Structure action) {
-        //System.out.println("\n["+ag+"] doing: "+action+"\n");
         boolean result = false;
-        if (action.equals(openFridgeParancs)) 
-        { // openFridgeParancs = open(fridge)
+        if (action.equals(openFridgeParancs)) { 
             result = model.openFridge();
-
         } 
-        else if (action.equals(closeFridgeParancs)) 
-        { // closeFridgeParancs = close(fridge)
+        else if (action.equals(closeFridgeParancs)) { 
             result = model.closeFridge();
-
         } 
-        else if (action.getFunctor().equals("move_towards"))
-        {
+        else if (action.getFunctor().equals("move_towards")){
             String l = action.getTerm(0).toString();
             Location dest = null;
             if (l.equals("fridge")) {
@@ -166,25 +147,20 @@ public class HouseEnv extends Environment {
                 result = true;
             }
         }
-        else if (action.equals(getAlapAgParancs))
-        {
+        else if (action.equals(getAlapAgParancs)){
             result = model.getAlapAg(currentKaja);
         } 
-        else if (action.equals(handInAlapAgParancs))
-        {
+        else if (action.equals(handInAlapAgParancs)){
             result = model.handInAlapAg();
         } 
-        else if (action.equals(kavarEtelParancs))
-        {
+        else if (action.equals(kavarEtelParancs)){
             result = model.kavarEtel();
         }
-        else if (action.equals(cookEtelParancs)) 
-        {
+        else if (action.equals(cookEtelParancs)) {
             result = model.cookFood();
         }
 
-        else if (action.getFunctor().equals("deliver")) 
-        {
+        else if (action.getFunctor().equals("deliver")) {
             // wait 3 seconds to finish "deliver"
             try {
                 Thread.sleep(3000);
@@ -208,7 +184,7 @@ public class HouseEnv extends Environment {
         } else if(action.equals(serveParancs)){
             result = true;
         } else if(action.equals(doneParancs)){
-            System.out.println("Kész a kaja");
+            System.out.println("Kész az étel");
             model.foodReady = true;
         }
         else 
